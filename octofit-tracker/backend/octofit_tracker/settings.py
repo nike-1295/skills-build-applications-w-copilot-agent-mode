eaaf1396b7b103c2854e9381565934de3857fd16
Nikhil Sreenivasan <Nikhilsreenivasan.1995@gmail.com>
2025-12-16 10:13:21 +0000

    Codespace/localhost URL support for API root and ALLOWED_HOSTS

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -26,8 +26,12 @@ SECRET_KEY = 'django-insecure-wi&t@^y5otrox@yt#f+kq(v0uudiqjaoxrw==hpjw%7wtegt@j
 DEBUG = True
 
 
-# Allow all hosts
-ALLOWED_HOSTS = ['*']
+# Allow codespace and localhost hosts
+import os
+CODESPACE_NAME = os.environ.get('CODESPACE_NAME')
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+if CODESPACE_NAME:
+    ALLOWED_HOSTS.append(f'{CODESPACE_NAME}-8000.app.github.dev')
 
 
 # Application definition
